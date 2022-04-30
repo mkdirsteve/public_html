15fc3e98faebccedd9013da8678275e880719f07
mkdirsteve <git@mkdirsteve.com>
2022-04-30 14:46:04 -0400

    new metaverse button

--- src/components/SocialLinksContainer.vue
+++ src/components/SocialLinksContainer.vue
@@ -1,69 +1,82 @@
 <template>
-    <v-container >
-<v-row>
-    <v-col v-for="link in links" align="center" :key="link.text">
-        <v-btn @click="openLink(link.url)">
-            <v-icon left>
-                {{link.icon}}
-            </v-icon>
-        {{link.text}}
+  <v-container>
+    <v-row>
+      <v-col class="justify-center text-center">
+        <v-btn @click="openLink('https://muse.place/atlas7')">
+          <v-icon left> {{ mdiVideo3d }} </v-icon> Metaverse
         </v-btn>
-    </v-col>
+      </v-col>
     </v-row>
-    </v-container>
+    <v-row>
+      <v-col v-for="link in links" align="center" :key="link.text">
+        <v-btn @click="openLink(link.url)">
+          <v-icon left>
+            {{ link.icon }}
+          </v-icon>
+          {{ link.text }}
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Vue from "vue"
-import { mdiGithub, mdiInstagram, mdiLinkedin, mdiTwitter, mdiYoutube } from "@mdi/js"
+import Vue from "vue";
+import {
+  mdiGithub,
+  mdiInstagram,
+  mdiLinkedin,
+  mdiTwitter,
+  mdiVideo3d,
+  mdiYoutube,
+} from "@mdi/js";
 
-export default Vue.extend ({
-    name:"SocialLinksContainer",
-    setup() {
-        return {
-            
-        }
+export default Vue.extend({
+  name: "SocialLinksContainer",
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      mdiVideo3d,
+      links: [
+        {
+          text: "Linkedin",
+          icon: mdiLinkedin,
+          url: "https://www.linkedin.com/in/stevengillen-ge/",
+        },
+        {
+          text: "Twitter",
+          icon: mdiTwitter,
+          url: "https://twitter.com/VenG_NFT",
+        },
+        {
+          text: "youtube",
+          icon: mdiYoutube,
+          url: "https://www.youtube.com/channel/UCmkWiS2LZblze2wrxX3ZJfQ",
+        },
+        // {
+        //     text: "Facebook",
+        //     icon: mdiFacebook,
+        // },
+        {
+          text: "GitHub",
+          icon: mdiGithub,
+          url: "https://github.com/mkdirsteve",
+        },
+        {
+          text: "Instagram",
+          icon: mdiInstagram,
+          url: "https://www.instagram.com/veng_nft/?hl=en",
+        },
+      ],
+    };
+  },
+  methods: {
+    openLink(url) {
+      if (!url) return;
+      window.open(url, "_blank");
     },
-    data(){
-        return {
-            links: [
-                {
-                    text: "Linkedin",
-                    icon: mdiLinkedin,
-                    url: "https://www.linkedin.com/in/stevengillen-ge/",
-                },
-                {
-                    text: "Twitter",
-                    icon: mdiTwitter,
-                    url: "https://twitter.com/VenG_NFT",
-                },
-                {
-                    text: "youtube",
-                    icon: mdiYoutube,
-                    url: "https://www.youtube.com/channel/UCmkWiS2LZblze2wrxX3ZJfQ",
-                },
-                // {
-                //     text: "Facebook",
-                //     icon: mdiFacebook,
-                // },
-                {
-                    text: "GitHub",
-                    icon: mdiGithub,
-                    url: "https://github.com/mkdirsteve"
-                },
-                {
-                    text: "Instagram",
-                    icon: mdiInstagram,
-                    url: "https://www.instagram.com/veng_nft/?hl=en"
-                },
-            ]
-        }
-    },
-    methods: {
-        openLink(url) {
-            if (!url) return ;
-            window.open(url, "_blank");
-        }
-    }
-})
+  },
+});
 </script>
