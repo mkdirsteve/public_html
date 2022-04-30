9e5d46d2d8c292ab28efeff36c81ed0c8a5d2a9f
mkdirsteve <git@mkdirsteve.com>
2022-04-30 15:37:35 -0400

    updated name

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+  watch: {
+    $route: {
+      immediate: true,
+      handler(to, _) {
+        document.title = to.meta.title || "MkdirSteve";
+      },
+    },
+  },
+}).$mount("#app");
